a3f47291ed7ca85af67ad91bcfbd4c433d1500a2
Trevor Richard <trmidpoint@gmail.com>
2023-08-30 05:19:10 +0000

    remove names of return vars

--- src/libraries/ExecutorParserLib.sol
+++ src/libraries/ExecutorParserLib.sol
@@ -8,33 +8,39 @@ pragma solidity ^0.8.19;
 library ExecutorParserLib {
 
   /// @notice Parses the message ID from `msg.data`.
-  /// @return _messageId The bytes32 message ID uniquely identifying the message that was executed
-  function messageId() internal pure returns (bytes32 _messageId) {
+  /// @return The bytes32 message ID uniquely identifying the message that was executed
+  function messageId() internal pure returns (bytes32) {
+    bytes32 _messageId;
     if (msg.data.length >= 84) {
       assembly {
         _messageId := calldataload(sub(calldatasize(), 84))
       }
     }
+    return _messageId;
   }
 
   /// @notice Parses the from chain ID from `msg.data`.
-  /// @return _fromChainId ID of the chain that dispatched the messages
-  function fromChainId() internal pure returns (uint256 _fromChainId) {
+  /// @return ID of the chain that dispatched the messages
+  function fromChainId() internal pure returns (uint256) {
+    uint256 _fromChainId;
     if (msg.data.length >= 52) {
       assembly {
         _fromChainId := calldataload(sub(calldatasize(), 52))
       }
     }
+    return _fromChainId;
   }
 
   /// @notice Parses the sender address from `msg.data`.
-  /// @return _sender The payable sender address
-  function msgSender() internal pure returns (address payable _sender) {
+  /// @return The payable sender address
+  function msgSender() internal pure returns (address payable) {
+    address payable _sender;
     if (msg.data.length >= 20) {
       assembly {
         _sender := shr(96, calldataload(sub(calldatasize(), 20)))
       }
     }
+    return _sender;
   }
 
 }
